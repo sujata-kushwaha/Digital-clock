efac52d2b02c2a4ff699dfc1b52a4e83d267f99f
sujata kushwaha <83424946+sujata-kushwaha@users.noreply.github.com>
2021-06-04 20:31:16 +0530

    Update digitalclock.py

--- digitalclock.py
+++ digitalclock.py
@@ -9,7 +9,7 @@ def time():
     string=strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000,time)
-label=Label(root,font=("ds-digital",60),background="black",foreground="red")
+label=Label(root,font=("ds-digital",60),background="black",foreground="red") #download the digital font 
 label.pack()
 button=Button(root,text="Close",width=15,command=root.destroy)
 button.pack()
